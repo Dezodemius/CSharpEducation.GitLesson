55790e222fc0e30fee800314eff96173db314e33
Гладков Егор <gladkovyegor@gmail.com>
2024-02-20 20:04:23 +0400

    Добавил решение

---
+++ CSharpEducation/CSharpEducation.GitLesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpEducation.GitLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
