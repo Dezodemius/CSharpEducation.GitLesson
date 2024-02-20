3459a86e3c330d3c11156055a2948b2151b7c446
Гладков Егор <gladkovyegor@gmail.com>
2024-02-20 20:45:40 +0400

    Убрал лишнее

--- CSharpEducation/Lesson2/Program.cs
+++ CSharpEducation/Lesson2/Program.cs
@@ -4,24 +4,7 @@
     {
         static void Main(string[] args)
         {
-            const int N = 1300;
-            var array = GetArray(N);
-            for (int i = 0; i < N; i++) 
-            {
-                array[i] = i;
-            }
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write(i);
-                if (i < N - 1)
-                    Console.Write(", ");
-            }
-            Console.WriteLine();
-        }
-
-        public static int[] GetArray(int n)
-        {
-            return new int[n];
+            
         }
     }
 }
