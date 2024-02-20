5f596f0d090cd1a46c9414f7a5bdbe9dde0c84f4
Polina Kozlova <158488988+Anoremia@users.noreply.github.com>
2024-02-20 20:28:19 +0400

    Create Program.cs
    5

---
+++ Polina/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task17
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            GetName();
+        }
+        public static string GetName()
+        {
+            return string.Empty;
+        }
+
+        
+    }
+}
