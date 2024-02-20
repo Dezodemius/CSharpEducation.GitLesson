3cf9d9b32434cb9da4f437c2a803e26caa4a1968
Гладков Егор <gladkovyegor@gmail.com>
2024-02-20 20:42:08 +0400

    Update Program.cs

--- CSharpEducation/Lesson2/Program.cs
+++ CSharpEducation/Lesson2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            const int N = 101;
+            const int N = 100;
             var array = GetArray(N);
             for (int i = 0; i < N; i++) 
             {
